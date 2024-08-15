04760f143053c4aa4e754ece2fcffa73f0f1b454
Bettenoch <bettenoch254@gmail.com>
2024-08-15 14:00:56 +0300

    Create cuatom loader/

---
+++ components/Loader.tsx
@@ -0,0 +1,11 @@
+
+//src/components/Loader.tsx
+const Loader = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="spinner">
+            </div>
+        </div>
+    );
+}
+export default Loader;
